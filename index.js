16ef5841d3dc5bae48bd3a261837a584cb186333
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-09 21:35:12 -0400

    sets the variables

---
+++ index.js
@@ -0,0 +1,6 @@
+let displayValElement = document.getElementById('calc-display-val');
+let clearBtn = document.getElementById('calc-clear');
+let backSpaceBtn = document.getElementById('calc-backspace');
+let decimalBtn = document.getElementById('calc-decimal');
+let calcNumBtns = document.getElementsByClassName('calc-num-btn');
+let calcOperatorBtns = document.getElementsByClassName('calc-btn-operator');
